db03e18faea318fe63863b45a06d3b2b0c207eb6
Your Name <you@example.com>
2020-01-13 22:51:24 -0500

    nicese

--- server.js
+++ server.js
@@ -1,13 +1,49 @@
+'use strict';
+
+const express = require('express');
+const { Server } = require('ws');
 const WebSocket = require('ws');
+const PORT = process.env.PORT || 5000;
+const INDEX = '/index.html';
 
-const wss = new WebSocket.Server({ port: 3030 });
+const server = express()
+    .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+    .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
+
+
+
+
+
+
+//const wss = new WebSocket.Server(undefined, );
+const wss = new WebSocket.Server({ port: 5030 });
+console.log("websocket server created");
 wss.on('connection', function connection(ws) {
+
+
     ws.on('message', function incoming(data) {
+        // eslint-disable-next-line no-unused-vars
+
+
+        // eslint-disable-next-line no-console
+        console.log("websocket connection open");
         wss.clients.forEach(function each(client) {
             if (client !== ws && client.readyState === WebSocket.OPEN) {
+                console.log(data);
                 client.send(data);
+
             }
         });
+
+
+
+
+        wss.on("close", function() {
+            // eslint-disable-next-line no-console
+            console.log("websocket connection close");
+
+        })
+
     });
 });
